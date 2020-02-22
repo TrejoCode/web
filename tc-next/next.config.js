a62be4f056485d0fc777cbccf220821ab9bb6612
Sergio Trejo <alejandrotrejocode@gmail.com>
2020-02-22 00:28:14 -0500

    ** Try to install Next-PWA

--- tc-next/next.config.js
+++ tc-next/next.config.js
@@ -1,8 +1,8 @@
 /**
- * @description Next.js config for SASS
- * @author trejocode
  * @version 1.0.0
+ * @author trejocode
+ * @description Next.js config for SASS and PWA
 */
 
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({});
+const withSass      = require('@zeit/next-sass');
+module.exports      = withSass({});
