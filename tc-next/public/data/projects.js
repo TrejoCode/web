8d7d384075cf19f5845795c20b00978614189ba1
Sergio Trejo <setrejo@palaceresorts.com>
2020-02-26 17:57:56 -0500

    Projectos en JSON

---
+++ tc-next/public/data/projects.js
@@ -0,0 +1,106 @@
+const projects = {
+    projects: [            
+        {
+            name: "Best Line Cancún",
+            slug: "www.bestlinecancun.com",
+            url: "https://www.bestlinecancun.com",
+            img: "/img/bestlinecancun.png",
+            services: [
+                "Diseño Web",
+                "Desarrollo Web"
+            ]
+        },
+        {
+            name: "Core.css",
+            slug: "github.com/TrejoCode/css",
+            url: "https://github.com/TrejoCode/css",
+            img: "/img/corecss.png",
+            services: [
+                "Open Source",
+                "Diseño Web"
+            ]
+        },
+        {
+            name: "CRM Altura",
+            slug: "crm.alturacancun.com",
+            url: "https://crm.alturacancun.com",
+            img: "/img/alturacancun.png",
+            services: [
+                "Desarrollo Web",
+                "Desarrollo PWA"
+            ]
+        },
+        {
+            name: "React Boilerplate",
+            slug: "github.com/TrejoCode/react-boilerplate",
+            url: "https://github.com/TrejoCode/react-boilerplate",
+            img: "/img/react-boilerplate.png",
+            services: [
+                "Open Source",
+                "Desarrollo Web"
+            ]
+        },
+        {
+            name: "Absolem Marketing",
+            slug: "www.absolemarketing.com.mx",
+            url: "https://absolemarketing.com.mx",
+            img: "/img/absolem.png",
+            services: [
+                "Desarrollo Web",
+                "Diseño Web"
+            ]
+        },
+        {
+            name: "Buenos Días",
+            slug: "www.buenosdiasmexico.mx",
+            url: "https://www.buenosdiasmexico.mx",
+            img: "/img/buenos-dias.png",
+            services: [
+                "Diseño Web",
+                "Desarrollo Web"
+            ]
+        },
+        {
+            name: "De la Luz",
+            slug: "www.delaluz.com.mx",
+            url: "https://www.delaluz.com.mx",
+            img: "/img/delaluz.png",
+            services: [
+                "Diseño Web",
+                "Desarrollo Web"
+            ]
+        },
+        {
+            name: "Elfos Gourmet",
+            slug: "www.elfosgourmet.eu",
+            url: "https://www.elfosgourmet.eu",
+            img: "/img/elfos-gourmet.png",
+            services: [
+                "Diseño Web",
+                "Desarrollo Web"
+            ]
+        },
+        {
+            name: "iGate",
+            slug: "www.igate.mx",
+            url: "https://play.google.com/store/apps/details?id=com.mobile.igate",
+            img: "/img/igate.png",
+            services: [
+                "App móvil",
+                "Desarrollo Web"
+            ]
+        },
+        {
+            name: "BéCé Online",
+            slug: "www.beceonline.com",
+            url: "https://www.beceonline.com",
+            img: "/img/bece.png",
+            services: [                
+                "Desarrollo Web",
+                "Diseño Web",
+            ]
+        },
+    ]
+};
+
+export default projects;
