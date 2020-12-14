57f407bb88cc08a157fef4d87b6623ecdbb17a24
Sergio Trejo <alejandrotrejocode@gmail.com>
2020-12-14 10:19:02 -0500

    🩹 Lista de Páginas exportadas

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/portafolio': { page: '/portafolio' },
+            '/politicas': { page: '/politicas' }
+        }
+    }
+}
